771bfe4055418b73a6b3385318a95f9aa397c274
Ranier Montalbo <ranie.montalbo@microapps.com>
2016-09-11 00:07:57 +0800

    Project config and setup files

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,162 +1,209 @@
-import webpack from 'webpack'
-import cssnano from 'cssnano'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
-import ExtractTextPlugin from 'extract-text-webpack-plugin'
-import config from '../config'
-import _debug from 'debug'
+import webpack from 'webpack';
+import cssnano from 'cssnano';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import ExtendedDefinePlugin from 'extended-define-webpack-plugin';
+import S3Plugin from 'webpack-s3-plugin';
+import config from '../config';
+import _debug from 'debug';
 
-const debug = _debug('app:webpack:config')
-const paths = config.utils_paths
-const { __DEV__, __PROD__, __TEST__ } = config.globals
+const debug = _debug('app:webpack:config');
+const paths = config.utils_paths;
+const {__DEV__, __PROD__, __TEST__} = config.globals;
 
-debug('Create configuration.')
+debug('Create configuration.');
 const webpackConfig = {
-  name    : 'client',
-  target  : 'web',
-  devtool : config.compiler_devtool,
-  resolve : {
-    root       : paths.client(),
-    extensions : ['', '.js', '.jsx', '.json']
+  name: 'client',
+  target: 'web',
+  devtool: config.compiler_devtool,
+  resolve: {
+    modules: [paths.client(), 'node_modules'],
+    extensions: ['', '.js', '.jsx', '.json']
   },
-  module : {}
-}
+  browser: {
+    fs: false
+  },
+  module: {},
+  externals: {
+    jquery: 'jQuery'
+  }
+};
 // ------------------------------------
 // Entry Points
 // ------------------------------------
 const APP_ENTRY_PATHS = [
   paths.client('main.js')
-]
+];
 
 webpackConfig.entry = {
-  app : __DEV__
+  app: __DEV__
     ? APP_ENTRY_PATHS.concat(`webpack-hot-middleware/client?path=${config.compiler_public_path}__webpack_hmr`)
     : APP_ENTRY_PATHS,
-  vendor : config.compiler_vendor
-}
+  vendor: config.compiler_vendor
+};
 
 // ------------------------------------
 // Bundle Output
 // ------------------------------------
 webpackConfig.output = {
-  filename   : `[name].[${config.compiler_hash_type}].js`,
-  path       : paths.dist(),
-  publicPath : config.compiler_public_path
-}
+  filename: `[name].[${config.compiler_hash_type}].js`,
+  path: paths.dist(),
+  publicPath: config.compiler_public_path,
+  devtoolModuleFilenameTemplate: config.compiler_devtool_filename_tempalte
+};
 
 // ------------------------------------
 // Plugins
 // ------------------------------------
 webpackConfig.plugins = [
-  new webpack.DefinePlugin(config.globals),
+  new ExtendedDefinePlugin(config.globals),
   new HtmlWebpackPlugin({
-    template : paths.client('index.html'),
-    hash     : false,
-    favicon  : paths.client('static/favicon.ico'),
-    filename : 'index.html',
-    inject   : 'body',
-    minify   : {
-      collapseWhitespace : true
+    template: paths.client('index.html'),
+    hash: false,
+    favicon: paths.client('static/favicon.png'),
+    filename: 'index.html',
+    inject: 'body',
+    minify: {
+      collapseWhitespace: true
     }
-  })
-]
+  }),
+  new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/)
+];
 
 if (__DEV__) {
-  debug('Enable plugins for live development (HMR, NoErrors).')
+  debug('Enable plugins for live development (HMR, NoErrors).');
   webpackConfig.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
-  )
+  );
 } else if (__PROD__) {
-  debug('Enable plugins for production (OccurenceOrder, Dedupe & UglifyJS).')
+  debug('Enable plugins for production (AggressiveMerging, OccurenceOrder, Dedupe & UglifyJS).');
   webpackConfig.plugins.push(
+    new webpack.optimize.AggressiveMergingPlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.DedupePlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
     new webpack.optimize.UglifyJsPlugin({
-      compress : {
-        unused    : true,
-        dead_code : true,
-        warnings  : false
+      compress: {
+        unused: true,
+        screw_ie8: true,
+        dead_code: true,
+        warnings: false
       }
     })
-  )
+  );
 }
 
-// Don't split bundles during testing, since we only want import one bundle
+if (config.deployToS3) {
+  debug(`Deploing to s3 in ${config.stage}`);
+  webpackConfig.plugins.push(
+    new S3Plugin(config.S3Plugin)
+  );
+}
+
+// Don't split bundles during testing, since we only want Import one bundle
 if (!__TEST__) {
   webpackConfig.plugins.push(
     new webpack.optimize.CommonsChunkPlugin({
-      names : ['vendor']
+      names: ['vendor']
     })
-  )
+  );
 }
 
 // ------------------------------------
 // Pre-Loaders
 // ------------------------------------
 /*
-[ NOTE ]
-We no longer use eslint-loader due to it severely impacting build
-times for larger projects. `npm run lint` still exists to aid in
-deploy processes (such as with CI), and it's recommended that you
-use a linting plugin for your IDE in place of this loader.
+ [ NOTE ]
+ We no longer use eslint-loader due to it severely impacting build
+ times for larger projects. `npm run lint` still exists to aid in
+ deploy processes (such as with CI), and it's recommended that you
+ use a linting plugin for your IDE in place of this loader.
 
-If you do wish to continue using the loader, you can uncomment
-the code below and run `npm i --save-dev eslint-loader`. This code
-will be removed in a future release.
+ If you do wish to continue using the loader, you can uncomment
+ the code below and run `npm i --save-dev eslint-loader`. This code
+ will be removed in a future release.
 
-webpackConfig.module.preLoaders = [{
-  test: /\.(js|jsx)$/,
-  loader: 'eslint',
-  exclude: /node_modules/
-}]
+ webpackConfig.module.preLoaders = [{
+ test: /\.(js|jsx)$/,
+ loader: 'eslint',
+ exclude: /node_modules/
+ }]
 
-webpackConfig.eslint = {
-  configFile: paths.base('.eslintrc'),
-  emitWarning: __DEV__
-}
-*/
+ webpackConfig.eslint = {
+ configFile: paths.base('.eslintrc'),
+ emitWarning: __DEV__
+ }
+ */
 
 // ------------------------------------
 // Loaders
 // ------------------------------------
 // JavaScript / JSON
-webpackConfig.module.loaders = [{
-  test    : /\.(js|jsx)$/,
-  exclude : /node_modules/,
-  loader  : 'babel',
-  query   : {
-    cacheDirectory : true,
-    plugins        : ['transform-runtime'],
-    presets        : ['es2015', 'react', 'stage-0']
+webpackConfig.module.loaders = [
+  {
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules\/(?!moment\/src)/,
+    loader: 'babel',
+    query: {
+      cacheDirectory: true,
+      plugins: [
+        ['transform-runtime', {
+          polyfill: false
+        }]
+      ],
+      presets: [
+        ['es2015', {
+          modules: false,
+          loose: true
+        }],
+        'react',
+        'stage-0'
+      ],
+      env: {
+        production: {
+          presets: ['react-optimize'],
+          compact: true
+        },
+        test: {
+          plugins: [
+            'rewire'
+          ]
+        }
+      }
+    }
+  },
+  {
+    test: /\.json$/,
+    loader: 'json'
   }
-},
-{
-  test   : /\.json$/,
-  loader : 'json'
-}]
+];
 
 // ------------------------------------
 // Style Loaders
 // ------------------------------------
 // We use cssnano with the postcss loader, so we tell
 // css-loader not to duplicate minimization.
-const BASE_CSS_LOADER = 'css?sourceMap&-minimize'
+const BASE_CSS_LOADER = 'css?sourceMap&-minimize';
 
 // Add any packge names here whose styles need to be treated as CSS modules.
 // These paths will be combined into a single regex.
 const PATHS_TO_TREAT_AS_CSS_MODULES = [
   // 'react-toolbox', (example)
-]
+];
 
 // If config has CSS modules enabled, treat this project's styles as CSS modules.
 if (config.compiler_css_modules) {
   PATHS_TO_TREAT_AS_CSS_MODULES.push(
     paths.client().replace(/[\^\$\.\*\+\-\?\=\!\:\|\\\/\(\)\[\]\{\}\,]/g, '\\$&') // eslint-disable-line
-  )
+  );
 }
 
-const isUsingCSSModules = !!PATHS_TO_TREAT_AS_CSS_MODULES.length
-const cssModulesRegex = new RegExp(`(${PATHS_TO_TREAT_AS_CSS_MODULES.join('|')})`)
+const isUsingCSSModules = !!PATHS_TO_TREAT_AS_CSS_MODULES.length;
+const cssModulesRegex = new RegExp(`(${PATHS_TO_TREAT_AS_CSS_MODULES.join('|')})`);
 
 // Loaders for styles that need to be treated as CSS modules.
 if (isUsingCSSModules) {
@@ -165,89 +212,123 @@ if (isUsingCSSModules) {
     'modules',
     'importLoaders=1',
     'localIdentName=[name]__[local]___[hash:base64:5]'
-  ].join('&')
+  ].join('&');
 
   webpackConfig.module.loaders.push({
-    test    : /\.scss$/,
-    include : cssModulesRegex,
-    loaders : [
+    test: /\.scss$/,
+    include: cssModulesRegex,
+    loaders: [
       'style',
       cssModulesLoader,
       'postcss',
       'sass?sourceMap'
     ]
-  })
+  });
 
   webpackConfig.module.loaders.push({
-    test    : /\.css$/,
-    include : cssModulesRegex,
-    loaders : [
+    test: /\.css$/,
+    include: cssModulesRegex,
+    loaders: [
       'style',
       cssModulesLoader,
       'postcss'
     ]
-  })
+  });
 }
 
 // Loaders for files that should not be treated as CSS modules.
-const excludeCSSModules = isUsingCSSModules ? cssModulesRegex : false
+const excludeCSSModules = isUsingCSSModules ? cssModulesRegex : false;
 webpackConfig.module.loaders.push({
-  test    : /\.scss$/,
-  exclude : excludeCSSModules,
-  loaders : [
+  test: /\.scss$/,
+  exclude: excludeCSSModules,
+  loaders: [
     'style',
     BASE_CSS_LOADER,
     'postcss',
     'sass?sourceMap'
   ]
-})
+});
 webpackConfig.module.loaders.push({
-  test    : /\.css$/,
-  exclude : excludeCSSModules,
-  loaders : [
+  test: /\.css$/,
+  exclude: excludeCSSModules,
+  loaders: [
     'style',
     BASE_CSS_LOADER,
     'postcss'
   ]
-})
+});
 
 // ------------------------------------
 // Style Configuration
 // ------------------------------------
 webpackConfig.sassLoader = {
-  includePaths : paths.client('styles')
-}
+  includePaths: paths.client('styles')
+};
 
 webpackConfig.postcss = [
   cssnano({
-    autoprefixer : {
-      add      : true,
-      remove   : true,
-      browsers : ['last 2 versions']
+    autoprefixer: {
+      add: true,
+      remove: true,
+      browsers: ['last 2 versions']
     },
-    discardComments : {
-      removeAll : true
+    discardComments: {
+      removeAll: true
     },
-    discardUnused : false,
-    mergeIdents   : false,
-    reduceIdents  : false,
-    safe          : true,
-    sourcemap     : true
+    discardUnused: false,
+    mergeIdents: false,
+    reduceIdents: false,
+    safe: true,
+    sourcemap: true
   })
-]
+];
 
 // File loaders
 /* eslint-disable */
 webpackConfig.module.loaders.push(
-  { test: /\.woff(\?.*)?$/,  loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff' },
-  { test: /\.woff2(\?.*)?$/, loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff2' },
-  { test: /\.otf(\?.*)?$/,   loader: 'file?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=font/opentype' },
-  { test: /\.ttf(\?.*)?$/,   loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/octet-stream' },
-  { test: /\.eot(\?.*)?$/,   loader: 'file?prefix=fonts/&name=[path][name].[ext]' },
-  { test: /\.svg(\?.*)?$/,   loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=image/svg+xml' },
-  { test: /\.(png|jpg)$/,    loader: 'url?limit=8192' }
-)
+  {
+    test: /\.woff(\?.*)?$/,
+    loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff'
+  },
+  {
+    test: /\.woff2(\?.*)?$/,
+    loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff2'
+  },
+  {test: /\.otf(\?.*)?$/, loader: 'file?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=font/opentype'},
+  {
+    test: /\.ttf(\?.*)?$/,
+    loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/octet-stream'
+  },
+  {test: /\.eot(\?.*)?$/, loader: 'file?prefix=fonts/&name=[path][name].[ext]'},
+  {test: /\.svg(\?.*)?$/, loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=image/svg+xml'},
+  {test: /\.(png|jpg)$/, loader: 'url?limit=8192'}
+);
 /* eslint-enable */
+
+// ------------------------------------
+// Auth0Lock configuration
+// ------------------------------------
+webpackConfig.module.loaders.push({
+  test: /node_modules[\\\/]auth0-lock[\\\/].*\.js$/,
+  loaders: [
+    'transform-loader/cacheable?brfs',
+    'transform-loader/cacheable?packageify'
+  ]
+}, {
+  test: /node_modules[\\\/]auth0-lock[\\\/].*\.ejs$/,
+  loader: 'transform-loader/cacheable?ejsify'
+});
+
+// ------------------------------------
+// AWS configuration
+// ------------------------------------
+process.env.AWS_SERVICES = config.aws_services.join(',');
+webpackConfig.module.loaders.push({
+  test: /node_modules[\\\/]aws-sdk/,
+  loaders: [
+    'transform-loader?aws-sdk/dist-tools/transform'
+  ]
+});
 
 // ------------------------------------
 // Finalize Configuration
@@ -255,21 +336,25 @@ webpackConfig.module.loaders.push(
 // when we don't know the public path (we know it only when HMR is enabled [in development]) we
 // need to use the extractTextPlugin to fix this issue:
 // http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809
-if (!__DEV__) {
-  debug('Apply ExtractTextPlugin to CSS loaders.')
+if (__PROD__) {
+  debug('Apply ExtractTextPlugin to CSS loaders.');
   webpackConfig.module.loaders.filter((loader) =>
     loader.loaders && loader.loaders.find((name) => /css/.test(name.split('?')[0]))
   ).forEach((loader) => {
-    const [first, ...rest] = loader.loaders
-    loader.loader = ExtractTextPlugin.extract(first, rest.join('!'))
-    Reflect.deleteProperty(loader, 'loaders')
-  })
+    const [first, ...rest] = loader.loaders;
+    loader.loader = ExtractTextPlugin.extract({
+      fallbackLoader: first,
+      loader: rest.join('!')
+    });
+    Reflect.deleteProperty(loader, 'loaders');
+  });
 
   webpackConfig.plugins.push(
-    new ExtractTextPlugin('[name].[contenthash].css', {
-      allChunks : true
+    new ExtractTextPlugin({
+      filename: '[name].[contenthash].css',
+      allChunks: true
     })
-  )
+  );
 }
 
-export default webpackConfig
+export default webpackConfig;
---
+++ config/stages.js
@@ -0,0 +1,64 @@
+// Here is where you can define configuration overrides based on the execution stage.
+// Supply a key to the default export matching the STAGE that you wish to target, and
+// the base configuration will apply your overrides before exporting itself.
+export default {
+  // ======================================================
+  // Overrides when STAGE === 'development'
+  // ======================================================
+  development: (config) => {
+    const region = 'us-east-1';
+    Object.assign(config.globals.APP_CONFIG, {
+      apiBaseURL: 'https://oq60megra3.execute-api.us-east-1.amazonaws.com/monei/',
+      csvImportBucket: '',
+      staticCdnURL: 'http://monei-v2-dashboard-development.s3.amazonaws.com',
+      intercomID: 'd84d8u48',
+      stripeKey: '',
+      iotEndpoint: '',
+      region
+    });
+
+    config.S3Plugin = {
+      s3Options: {
+        accessKeyId: process.env.DEV_ACCESS_KEY_ID,
+        secretAccessKey: process.env.DEV_SECRET_ACCESS_KEY,
+        region
+      },
+      s3UploadOptions: {
+        Bucket: 'monei-v2-dashboard-development'
+      }
+    };
+    return config;
+  },
+
+  // ======================================================
+  // Overrides when STAGE === 'production'
+  // ======================================================
+  production: (config) => {
+    const region = 'eu-west-1';
+    Object.assign(config.globals.APP_CONFIG, {
+      apiBaseURL: '',
+      csvImportBucket: '',
+      staticCdnURL: '',
+      intercomID: '',
+      stripeKey: '',
+      iotEndpoint: '',
+      region
+    });
+
+    config.S3Plugin = {
+      s3Options: {
+        accessKeyId: process.env.PROD_ACCESS_KEY_ID,
+        secretAccessKey: process.env.PROD_SECRET_ACCESS_KEY,
+        region
+      },
+      s3UploadOptions: {
+        Bucket: ''
+      },
+      cloudfrontInvalidateOptions: {
+        DistributionId: '',
+        Items: ['/index.html']
+      }
+    };
+    return config;
+  }
+};
