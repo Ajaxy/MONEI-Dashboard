6944d78baf7089e05c0854f2bde5d37ece6978d7
Dmitriy Nevzorov <jimmy.lugat@gmail.com>
2016-10-27 18:44:04 +0300

    setup settings view

---
+++ src/routes/Profile/routes/Settings/components/SettingsView.js
@@ -0,0 +1,9 @@
+import React, {PropTypes} from 'react';
+
+const SettingsView = () => (
+  <section className="ui vertical segment">
+    <h3>Phone settings</h3>
+  </section>
+);
+
+export default SettingsView;
