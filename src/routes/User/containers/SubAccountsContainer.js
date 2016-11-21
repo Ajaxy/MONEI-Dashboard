aff31a001e66be1f410021004ebc0008d549232e
Dmitriy Nevzorov <jimmy.lugat@gmail.com>
2016-11-21 14:18:37 +0300

    fix sub accounts container

--- src/routes/User/containers/SubAccountsContainer.js
+++ src/routes/User/containers/SubAccountsContainer.js
@@ -4,18 +4,18 @@ import * as actions from '../modules/actions';
 import * as selectors from '../modules/selectors';
 import SubAccountsView from '../components/SubAccountsView';
 
-class User extends Component {
+class SubAccounts extends Component {
   static propTypes = {
     fetchSubAccounts: PropTypes.func.isRequired
   };
 
   componentDidMount() {
-    const {userId, fetchSubAccounts} = this.props;
-    fetchSubAccounts(userId);
+    const {user, fetchSubAccounts} = this.props;
+    fetchSubAccounts(user.user_id);
   }
 
   render() {
-    return <UserView{...this.props} />;
+    return <SubAccountsView{...this.props} />;
   }
 }
 
@@ -23,7 +23,7 @@ const mapStateToProps = (state, props) => ({
   user: selectors.getUser(state),
   subAccounts: selectors.getSubAccounts(state),
   isSyncing: selectors.getIsSyncing(state),
-  isFetching: selectors.getIsFetchingSubAccounts(state)
+  isFetching: selectors.getIsFetchingSubAccounts(state),
 });
 
-export default connect(mapStateToProps, actions)(SubAccountsView);
+export default connect(mapStateToProps, actions)(SubAccounts);
