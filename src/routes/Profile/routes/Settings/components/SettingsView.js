c94f64965fdd2d7a14709be6f47c90934cfa8d5b
Dmitriy Nevzorov <jimmy.lugat@gmail.com>
2016-12-07 16:11:47 +0300

    add new files

---
+++ src/routes/Profile/routes/Settings/components/SettingsView.js
@@ -0,0 +1,47 @@
+import React, {PropTypes} from 'react';
+import CheckBox from 'components/CheckBox';
+import DotHint from 'components/DotHint';
+
+const SettingsView = ({
+  fields: {
+    isHintsDisabled
+  },
+  handleSubmit,
+  updateProfileMetaData
+}) => {
+  const disableHints = (e) => {
+    updateProfileMetaData({isHintsDisabled: e.target.value !== 'true'});
+    isHintsDisabled.onChange(e);
+  };
+  return (
+    <section className="ui vertical segment">
+      <div className="ui stackable grid">
+        <div className="nine wide column">
+          <form className="ui form" onSubmit={handleSubmit(updateProfileMetaData)}>
+            <h3>Application settings</h3>
+            <div className="field">
+              <CheckBox
+                {...isHintsDisabled}
+                className="green"
+                onChange={disableHints}
+                toggle
+                label="Disable application hints"
+              />
+              <DotHint>
+                This is an example of the hint
+              </DotHint>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+SettingsView.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  updateProfileMetaData: PropTypes.func.isRequired,
+  fields: PropTypes.object.isRequired
+};
+
+export default SettingsView;
