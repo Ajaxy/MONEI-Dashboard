cedbf31c392b5c8594bdaebb480ca88ff632608b
Alexander Zinchuk <alx.zinchuk@gmail.com>
2016-12-18 03:09:15 +0300

    Widget callback url field added

--- src/routes/Widget/containers/WidgetView.js
+++ src/routes/Widget/containers/WidgetView.js
@@ -1,8 +1,5 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
 import classNames from './WidgetView.scss';
-// import Button from 'components/Button';
-// import Loader from 'components/Loader';
-import cx from 'classnames';
 
 const MIN_AMOUNT = 1;
 const MAX_AMOUNT = 999;
@@ -12,12 +9,17 @@ class WidgetView extends Component {
     super(props, context);
 
     this.state = {
-      amount: 5
+      amount: 50,
+      redirectUrl: 'yoursite.com/monei-callback'
     };
   }
 
   onChangeAmount(e) {
     this.setState({amount: e.target.value});
+  }
+
+  onChangeRedirectUrl(e) {
+    this.setState({redirectUrl: e.target.value});
   }
 
   render() {
@@ -28,23 +30,50 @@ class WidgetView extends Component {
           <div className="sub header">Easily collect payments from your customers</div>
         </h1>
         <h3>1. Insert this code between <code>&lt;head&gt;</code> tags:</h3>
-      <pre>
-        &lt;script type=&quot;text/javascript&quot; src=&quot;https://jsapi.monei.net/widget.js&quot;&gt;&lt;/script&gt;
-      </pre>
+        <pre>
+          &lt;script type=&quot;text/javascript&quot; src=&quot;https://jsapi.monei.net/widget.js&quot;&gt;&lt;
+          /script&gt;
+        </pre>
         <h3>2. Choose the amount you want to charge:</h3>
         <div className={classNames.amount}>
-          <input type="range"
-                 min={MIN_AMOUNT}
-                 max={MAX_AMOUNT}
-                 name="amount"
-                 onChange={this.onChangeAmount.bind(this)}
+          <div className="ui right labeled input">
+            <div className="ui label">&euro;</div>
+            <input
+              type="number"
+              min={MIN_AMOUNT}
+              max={MAX_AMOUNT}
+              value={this.state.amount}
+              onChange={this.onChangeAmount.bind(this)}
+            />
+            <div className="ui basic label">.00</div>
+          </div>
+          <input
+            type="range"
+            min={MIN_AMOUNT}
+            max={MAX_AMOUNT}
+            name="amount"
+            value={this.state.amount}
+            onChange={this.onChangeAmount.bind(this)}
           />
-          <span>&euro; {this.state.amount}</span>
         </div>
-        <h3>3. Insert this code in any place you want to display widget:</h3>
-      <pre>
-        &lt;div class=&quot;monei-widget&quot; data-amount=&quot;{this.state.amount}&quot;&gt;&lt;/div&gt;
-      </pre>
+        <h3>3. Setup the callback URL:</h3>
+        <div className="ui labeled fluid input">
+          <div className="ui label">
+            https://
+          </div>
+          <input
+            type="text"
+            value={this.state.redirectUrl}
+            onKeyUp={this.onChangeRedirectUrl.bind(this)}
+            // onKeyUp={this.onChangeRedirectUrl.bind(this)}
+          />
+        </div>
+        <h3>4. Insert this code in any place you want to put widget:</h3>
+        <pre>
+          &lt;div class=&quot;monei-widget&quot;
+          data-amount=&quot;{this.state.amount}&quot;
+          data-redirect-url=&quot;https://{this.state.redirectUrl}&quot;&gt;&lt;/div&gt;
+        </pre>
       </section>
     );
   }
